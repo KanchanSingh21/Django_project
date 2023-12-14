b5b498ff935d86514589d635b753860b8a102188
Kanchan Singh Kunwar <145453114+KanchanSingh21@users.noreply.github.com>
2023-12-14 20:17:00 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse
+from signup.models import signup_details
+from login.models import login_details
+
+def signup(request):
+    return render(request,'signup.html')
+
+def signup_info(request):
+    if request.method=='POST':
+
+        username=request.POST['username']
+        mydate=request.POST['mydate']
+        myemail=request.POST['myemail']
+        myaddress=request.POST['myaddress']
+        country=request.POST['country']
+        statename=request.POST['statename']
+        myGender=request.POST['myGender']
+        role=request.POST['role']
+        password=request.POST['password']
+        confirm_password=request.POST['confirm_password']
+
+        user=signup_details.objects.create(username=username,mydate=mydate,myemail=myemail,myaddress=myaddress,country=country,statename=statename,myGender=myGender,role=role,password=password,confirm_password=confirm_password)
+        return HttpResponse("you have successfully inserted your details in database")
+   
+    
+
+def login(request):
+    return render(request,'login.html')
+
+def login_info(request):
+    if request.method=='POST':
+
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=login_details.objects.create(username=username,password=password)
+        return HttpResponse("you have successfully inserted your details in database")
